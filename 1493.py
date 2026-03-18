e686568d74db561da97cc30459afc555b9397eb7
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-18 14:24:30 +0500

    prob:(1493) longest subArray

--- 1493.py
+++ 1493.py
@@ -20,3 +20,31 @@ Input: nums = [1,1,1]
 Output: 2
 Explanation: You must delete one element.
 """
+
+
+class Solution:
+    def longestSubarray(self, nums: list[int]) -> int:
+        left = 0
+        zeros = 0
+        max_len = 0
+        
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros += 1
+            
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+            
+            max_len = max(max_len, right - left + 1)
+        
+        return max_len - 1  # delete one element        
+    
+    
+if __name__ == "__main__":
+    sol = Solution()
+        
+    print(sol.longestSubarray([1,1,0,1]))           # 3
+    print(sol.longestSubarray([0,1,1,1,0,1,1,0,1])) # 5
+    print(sol.longestSubarray([1,1,1]))             # 2
