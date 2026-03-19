8fb8292950c6f012d31063e6e7a34ae9cf3510b5
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-19 23:43:46 +0500

    prob:(3212) submatrix

---
+++ 3212.py
@@ -0,0 +1,31 @@
+"""3212. Count Submatrices With Equal Frequency of X and Y
+
+Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
+grid[0][0]
+an equal frequency of 'X' and 'Y'.
+at least one 'X'.
+ 
+
+Example 1:
+Input: grid = [["X","Y","."],["Y",".","."]]
+Output: 3
+
+Example 2:
+Input: grid = [["X","X"],["X","Y"]]
+Output: 0
+
+for Explaination see the question on  leetcode and see the image that are explaining well
+
+No submatrix has an equal frequency of 'X' and 'Y'.
+
+Example 3:
+
+Input: grid = [[".","."],[".","."]]
+
+Output: 0
+
+Explanation:
+
+No submatrix has at least one 'X'."""
+
+ 
