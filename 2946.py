71dcee937b97ce58ba24ef059a99b3c2f0dfcfec
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-28 23:26:52 +0500

    prob:(2946) solution

--- 2946.py
+++ 2946.py
@@ -25,3 +25,23 @@ Output: true
 Explanation:
 As all the values are equal in the matrix, even after performing cyclic shifts the matrix will remain the same."""
 
+
+from typing import List
+
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        m, n = len(mat), len(mat[0])
+        k %= n
+        
+        for i in range(m):
+            for j in range(n):
+                if i % 2 == 0:
+                    if mat[i][j] != mat[i][(j + k) % n]:
+                        return False
+                else:
+                    if mat[i][j] != mat[i][(j - k) % n]:
+                        return False
+        
+        return True
+
+
