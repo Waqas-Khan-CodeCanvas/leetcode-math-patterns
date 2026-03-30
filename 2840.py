1b254b85021693e34ca093853140698bcdb3258d
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-31 00:04:03 +0500

    prob:(2840) solution

--- 2840.py
+++ 2840.py
@@ -20,3 +20,16 @@ Output: false
 Explanation: It is not possible to make the two strings equal.
  """
  
+ 
+from collections import Counter
+
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even1 = Counter(s1[i] for i in range(0, len(s1), 2))
+        even2 = Counter(s2[i] for i in range(0, len(s2), 2))
+        
+        odd1 = Counter(s1[i] for i in range(1, len(s1), 2))
+        odd2 = Counter(s2[i] for i in range(1, len(s2), 2))
+        
+        return even1 == even2 and odd1 == odd2
+
