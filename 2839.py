ce520ed1c7181a8d8ce8ba63b4ce614ff59701a7
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-30 23:58:30 +0500

    prob:(2839) solution

--- 2839.py
+++ 2839.py
@@ -20,3 +20,13 @@ Explanation: It is not possible to make the two strings equal.
  """
 
 
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        # Check even indices
+        if sorted([s1[0], s1[2]]) != sorted([s2[0], s2[2]]):
+            return False
+        # Check odd indices
+        if sorted([s1[1], s1[3]]) != sorted([s2[1], s2[3]]):
+            return False
+        return True
+
