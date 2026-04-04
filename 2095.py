faab8b19bc44022f3fd4a9606937404e128696d7
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-04-04 23:26:15 +0500

    prob:(2095) solution

--- 2095.py
+++ 2095.py
@@ -2,3 +2,48 @@
 # see the quetion on leetcode this inlcude digram that will help you to get better idea
 
 
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        
+        slow = head
+        fast = head
+        prev = None
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev.next = slow.next
+        return head
+
+
+# 🔽 Helper functions for testing
+def build_list(arr):
+    dummy = ListNode()
+    cur = dummy
+    for x in arr:
+        cur.next = ListNode(x)
+        cur = cur.next
+    return dummy.next
+
+
+def print_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    print(result)
+
+
