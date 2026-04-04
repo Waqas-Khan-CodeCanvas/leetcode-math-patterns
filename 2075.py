68b06ce11ace7a2a732337647592da4839bbf5f4
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-04-04 23:20:55 +0500

    prob:(2075) solution

--- 2075.py
+++ 2075.py
@@ -1,2 +1,28 @@
 # 2075. Decode the Slanted Ciphertext
 # see  more about this on leetcode this include image from which you will get better idea about this problem
+
+
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        if rows == 1:
+            return encodedText
+        
+        n = len(encodedText)
+        cols = n // rows
+        
+        matrix = []
+        idx = 0
+        for r in range(rows):
+            matrix.append(list(encodedText[idx:idx + cols]))
+            idx += cols
+        
+        result = []
+        for start_col in range(cols):
+            r, c = 0, start_col
+            while r < rows and c < cols:
+                result.append(matrix[r][c])
+                r += 1
+                c += 1
+        
+        return "".join(result).rstrip()
+
